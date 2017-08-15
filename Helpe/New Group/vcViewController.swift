6ceff2c195063df3cb7d775e0bb8c56aa9c523e7
liudong <dannyliudong@users.noreply.github.com>
2017-08-15 21:08:56 +0800

    new branch

---
+++ Helpe/New Group/vcViewController.swift
@@ -0,0 +1,35 @@
+//
+//  vcViewController.swift
+//  Helpe
+//
+//  Created by liudong on 2017/8/15.
+//  Copyright © 2017年 liudong. All rights reserved.
+//
+
+import UIKit
+
+class vcViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
